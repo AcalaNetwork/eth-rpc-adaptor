80162e6053ac6e815c9d96153dc5ba59e93f70b6
Bryan Chen <xlchen1291@gmail.com>
2021-10-07 01:00:09 +0000

    update

--- evm-subql/src/mappings/mappingHandlers.ts
+++ evm-subql/src/mappings/mappingHandlers.ts
@@ -2,77 +2,78 @@ import type { EvmLog, H160 } from '@polkadot/types/interfaces/types';
 import { SubstrateEvent } from '@subql/types';
 import { Log, TransactionReceipt } from '../types';
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-  if (event.event.section.toUpperCase() !== 'EVM') return;
+const NOT_EXIST_TRANSACTION_INDEX = 0xffffffff
 
+export async function handleEvmEvent(event: SubstrateEvent): Promise<void> {
   const { block } = event;
 
-  if (event.event.method.toUpperCase() === 'LOG') {
-    const evmLog = event.event.data[0] as EvmLog;
+  switch (event.event.method) {
+    case 'Log': {
+      const evmLog = event.event.data[0] as EvmLog;
 
-    const log = Log.create({
-      id: event.idx,
-      transactionHash: event.extrinsic.extrinsic.hash.toHex(),
-      blockNumber: block.block.header.number.toNumber(),
-      blockHash: block.block.hash,
-      transactionIndex: event.phase.toNumber(),
-      removed: false,
-      address: evmLog.address.toString().toLowerCase(),
-      data: evmLog.address.toString().toLowerCase(),
-      topics: evmLog.topics.toJSON(),
-      logIndex: event.idx,
-    });
+      const log = Log.create({
+        id: `${block.block.header.number.toString()}-${event.extrinsic?.idx ?? NOT_EXIST_TRANSACTION_INDEX}`,
+        transactionHash: event.extrinsic.extrinsic.hash.toHex(),
+        blockNumber: block.block.header.number.toNumber(),
+        blockHash: block.block.hash,
+        transactionIndex: event.phase.toNumber(),
+        removed: false,
+        address: evmLog.address.toString().toLowerCase(),
+        data: evmLog.address.toString().toLowerCase(),
+        topics: evmLog.topics.toJSON(),
+        logIndex: event.idx,
+        receiptId: 
+      });
+  
+      await log.save();
+    }
+    case 'Created':
+    case 'Executed': {
+      // EvmAddress
+      const evmAddress = event.event.data[0] as H160;
+  
+      const transactionReceipt = TransactionReceipt.create({
+        id: `${block.block.header.number.toString()}-${event.extrinsic?.idx ?? NOT_EXIST_TRANSACTION_INDEX}`,
+        to: evmAddress.toString(),
+        from: '', // @Todo
+        contractAddress: evmAddress.toString(),
+        transactionIndex: event.phase.isApplyExtrinsic ? event.extrinsic?.idx ?? NOT_EXIST_TRANSACTION_INDEX,
+        gasUsed: 0, // @Todo
+        logsBloom: '0x', // @Todo
+        blockHash: block.block.hash,
+        transactionHash: event.extrinsic.extrinsic.hash.toHex(),
+        blockNumber: block.block.header.number.toNumber(),
+        confirmations: 4, // @Todo
+        cumulativeGasUsed: 0, // @Todo
+        byzantium: false,
+        status: 1,
+      });
 
-    await log.save();
-  }
+      await transactionReceipt.save();
+    }
+    case 'CreatedFailed':
+    case 'ExecutedFailed': {
+      // EvmAddress, ExitReason, Bytes
+      const evmAddress = event.event.data[0] as H160;
 
-  // EvmAddress
-  if (['Created', 'Executed'].includes(event.event.method.toUpperCase())) {
-    const evmAddress = event.event.data[0] as H160;
-
-    const transactionReceipt = TransactionReceipt.create({
-      id: event.idx,
-      to: evmAddress.toString(),
-      from: '', // @Todo
-      contractAddress: evmAddress.toString(),
-      transactionIndex: event.phase.toNumber(),
-      gasUsed: 0, // @Todo
-      logsBloom: '0x', // @Todo
-      blockHash: block.block.hash,
-      transactionHash: event.extrinsic.extrinsic.hash.toHex(),
-      logs: [event.idx],
-      blockNumber: block.block.header.number.toNumber(),
-      confirmations: 4, // @Todo
-      cumulativeGasUsed: 0, // @Todo
-      byzantium: false,
-      status: 1,
-    });
-
-    await transactionReceipt.save();
-  }
-
-  // EvmAddress, ExitReason, Bytes
-  if (['CreatedFailed', 'ExecutedFailed'].includes(event.event.method.toUpperCase())) {
-    const evmAddress = event.event.data[0] as H160;
-
-    const transactionReceipt = TransactionReceipt.create({
-      id: event.idx,
-      to: evmAddress.toString(),
-      from: '', // @Todo
-      contractAddress: evmAddress.toString(),
-      transactionIndex: event.phase.toNumber(),
-      gasUsed: 0, // @Todo
-      logsBloom: '0x', // @Todo
-      blockHash: block.block.hash,
-      transactionHash: event.extrinsic.extrinsic.hash.toHex(),
-      logs: [event.idx],
-      blockNumber: block.block.header.number.toNumber(),
-      confirmations: 4, // @Todo
-      cumulativeGasUsed: 0, // @Todo
-      byzantium: false,
-      status: 0,
-    });
-
-    await transactionReceipt.save();
+      const transactionReceipt = TransactionReceipt.create({
+        id: `${block.block.header.number.toString()}-${event.extrinsic?.idx ?? NOT_EXIST_TRANSACTION_INDEX}`,
+        to: evmAddress.toString(),
+        from: '', // @Todo
+        contractAddress: evmAddress.toString(),
+        transactionIndex: event.phase.toNumber(),
+        gasUsed: 0, // @Todo
+        logsBloom: '0x', // @Todo
+        blockHash: block.block.hash,
+        transactionHash: event.extrinsic.extrinsic.hash.toHex(),
+        blockNumber: block.block.header.number.toNumber(),
+        confirmations: 4, // @Todo
+        cumulativeGasUsed: 0, // @Todo
+        byzantium: false,
+        status: 0,
+      });
+  
+      await transactionReceipt.save();
+    }
   }
 }
