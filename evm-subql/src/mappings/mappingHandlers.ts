fa4e95539b28b27b7e40a7005d56d2d898560c7e
Xiliang Chen <xlchen1291@gmail.com>
2021-10-08 14:38:41 +1300

    Fix evm-subql (#9)
    * update
    
    * update handlers
    
    * improve
    
    * update

--- evm-subql/src/mappings/mappingHandlers.ts
+++ evm-subql/src/mappings/mappingHandlers.ts
@@ -2,77 +2,119 @@ import type { EvmLog, H160 } from '@polkadot/types/interfaces/types';
 import { SubstrateEvent } from '@subql/types';
 import { Log, TransactionReceipt } from '../types';
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-  if (event.event.section.toUpperCase() !== 'EVM') return;
+const NOT_EXIST_TRANSACTION_INDEX = 0xffff;
 
+export async function handleEvmEvent(event: SubstrateEvent): Promise<void> {
   const { block } = event;
 
-  if (event.event.method.toUpperCase() === 'LOG') {
-    const evmLog = event.event.data[0] as EvmLog;
+  const txIdx = event.extrinsic?.idx ?? NOT_EXIST_TRANSACTION_INDEX;
 
+  const createLog = async (receiptId: string, idx: number, evmLog: EvmLog) => {
     const log = Log.create({
-      id: event.idx,
+      id: `${receiptId}-${idx}`,
       transactionHash: event.extrinsic.extrinsic.hash.toHex(),
       blockNumber: block.block.header.number.toNumber(),
       blockHash: block.block.hash,
-      transactionIndex: event.phase.toNumber(),
+      transactionIndex: txIdx,
       removed: false,
       address: evmLog.address.toString().toLowerCase(),
       data: evmLog.address.toString().toLowerCase(),
       topics: evmLog.topics.toJSON(),
-      logIndex: event.idx,
+      logIndex: idx,
+      receiptId,
     });
 
     await log.save();
+  };
+
+  const processEvent = (id: string) => {
+    switch (event.event.method) {
+      case 'Created': {
+        const [source, evmAddress, logs] = event.event.data as unknown as [H160, H160, EvmLog[]];
+
+        return [
+          TransactionReceipt.create({
+            id,
+            to: null,
+            from: source.toHex(),
+            contractAddress: evmAddress.toString(),
+            logsBloom: '0x', // @Todo
+            status: 1,
+          }),
+          logs,
+        ] as [TransactionReceipt, EvmLog[]];
+      }
+      case 'Executed': {
+        const [source, evmAddress, logs] = event.event.data as unknown as [H160, H160, EvmLog[]];
+
+        return [
+          TransactionReceipt.create({
+            id,
+            to: evmAddress.toString(),
+            from: source.toHex(),
+            contractAddress: evmAddress.toString(),
+            logsBloom: '0x', // @Todo
+            status: 1,
+          }),
+          logs,
+        ] as [TransactionReceipt, EvmLog[]];
+      }
+      case 'CreatedFailed': {
+        const [source, evmAddress, _exitReason, logs] = event.event.data as unknown as [H160, H160, unknown, EvmLog[]];
+
+        return [
+          TransactionReceipt.create({
+            id,
+            to: null,
+            from: source.toHex(),
+            contractAddress: evmAddress.toString(),
+            logsBloom: '0x', // @Todo
+            status: 0,
+          }),
+          logs,
+        ] as [TransactionReceipt, EvmLog[]];
+      }
+      case 'ExecutedFailed': {
+        const [source, evmAddress, _exitReason, _output, logs] = event.event.data as unknown as [
+          H160, 
+          H160,
+          unknown,
+          unknown,
+          EvmLog[]
+        ];
+
+        return [
+          TransactionReceipt.create({
+            id,
+            to: evmAddress.toString(),
+            from: source.toHex(),
+            contractAddress: null,
+            logsBloom: '0x', // @Todo
+            status: 0,
+          }),
+          logs,
+        ] as [TransactionReceipt, EvmLog[]];
+      }
+    }
+
+    return null;
+  };
+
+  const ret = processEvent(`${block.block.header.number.toString()}-${event.extrinsic?.idx ?? event.phase.toString()}`);
+  if (!ret) {
+    logger.debug(`Unsupported event: ${event.event.method}`);
+    return;
   }
 
-  // EvmAddress
-  if (['Created', 'Executed'].includes(event.event.method.toUpperCase())) {
-    const evmAddress = event.event.data[0] as H160;
+  const [transactionReceipt, logs] = ret;
+  transactionReceipt.blockHash = block.block.hash.toHex(),
+  transactionReceipt.transactionHash = event.extrinsic.extrinsic.hash.toHex(),
+  transactionReceipt.blockNumber = BigInt(block.block.header.number.toNumber()),
+  transactionReceipt.transactionIndex = BigInt(txIdx);
+  transactionReceipt.gasUsed = BigInt(0); // @Todo
+  transactionReceipt.cumulativeGasUsed = BigInt(0); // @Todo
 
-    const transactionReceipt = TransactionReceipt.create({
-      id: event.idx,
-      to: evmAddress.toString(),
-      from: '', // @Todo
-      contractAddress: evmAddress.toString(),
-      transactionIndex: event.phase.toNumber(),
-      gasUsed: 0, // @Todo
-      logsBloom: '0x', // @Todo
-      blockHash: block.block.hash,
-      transactionHash: event.extrinsic.extrinsic.hash.toHex(),
-      logs: [event.idx],
-      blockNumber: block.block.header.number.toNumber(),
-      confirmations: 4, // @Todo
-      cumulativeGasUsed: 0, // @Todo
-      byzantium: false,
-      status: 1,
-    });
+  logs.forEach((evmLog, idx) => createLog(transactionReceipt.id, idx, evmLog));
 
-    await transactionReceipt.save();
-  }
-
-  // EvmAddress, ExitReason, Bytes
-  if (['CreatedFailed', 'ExecutedFailed'].includes(event.event.method.toUpperCase())) {
-    const evmAddress = event.event.data[0] as H160;
-
-    const transactionReceipt = TransactionReceipt.create({
-      id: event.idx,
-      to: evmAddress.toString(),
-      from: '', // @Todo
-      contractAddress: evmAddress.toString(),
-      transactionIndex: event.phase.toNumber(),
-      gasUsed: 0, // @Todo
-      logsBloom: '0x', // @Todo
-      blockHash: block.block.hash,
-      transactionHash: event.extrinsic.extrinsic.hash.toHex(),
-      logs: [event.idx],
-      blockNumber: block.block.header.number.toNumber(),
-      confirmations: 4, // @Todo
-      cumulativeGasUsed: 0, // @Todo
-      byzantium: false,
-      status: 0,
-    });
-
-    await transactionReceipt.save();
-  }
+  await transactionReceipt.save();
 }
